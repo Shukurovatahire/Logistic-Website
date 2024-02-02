a3ae1cb91386cdb520bcecd221f5577678589c35
Shukurovatahire <156453047+Shukurovatahire@users.noreply.github.com>
2024-02-02 15:13:57 +0400

    Add files via upload

---
+++ LogisticWebsite/scrip.js
@@ -0,0 +1,28 @@
+document.getElementById('click-element').addEventListener('click', function () {
+    if (document.getElementById('hiddenText').style.display == 'block') {
+        document.getElementById('hiddenText').style.display = 'none';
+    }
+    else {
+        document.getElementById('hiddenText').style.display = 'block';
+    }
+})
+
+document.getElementById('click-element2').addEventListener('click', function () {
+    if (document.getElementById('hiddenText2').style.display == 'block') {
+        document.getElementById('hiddenText2').style.display = 'none';
+    }
+    else {
+        document.getElementById('hiddenText2').style.display = 'block';
+    }
+})
+
+
+document.getElementById('click-element3').addEventListener('click', function () {
+    if (document.getElementById('hiddenText3').style.display == 'block') {
+        document.getElementById('hiddenText3').style.display = 'none';
+    }
+    else {
+        document.getElementById('hiddenText3').style.display = 'block';
+    }
+})
+
